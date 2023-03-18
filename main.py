69f8d408bb0c48a1ea550f23eec7579d2c88971f
Aritra Bose <aritrabose2003@gmail.com>
2023-03-18 18:42:21 +0530

    re-transfer to render.com

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ bootstrap = Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
 # CONNECT TO DATABASE, CONFIGURE SQLALCHEMY
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL").replace("://", "ql://", 1)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL").replace("://", "ql://", 1)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
